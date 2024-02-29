6ff7c38fc7090ca717967778fa8b8d0e31f4ae01
Melisa <andream_dearth@hotmail.com>
2024-02-29 19:23:07 +0000

    testing if MUI and framer motion are well installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { motion } from "framer-motion"
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +33,17 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Button variant="contained">Hello world</Button>
+
+      <motion.div className="motion"
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        }}
+      />
+
     </>
   )
 }
