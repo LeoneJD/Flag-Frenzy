6e8e390c9556d526407eb9a440ef56f07b20de51
Melisa <andream_dearth@hotmail.com>
2024-03-01 22:45:55 +0000

    Adding the onChange event when selecting the continent

--- src/components/continentSelector.jsx
+++ src/components/continentSelector.jsx
@@ -1,23 +1,26 @@
-import { motion } from "framer-motion"
-
 import * as React from 'react';
+import { motion } from 'framer-motion';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-
 export default function ContinentSelector() {
+    const selectedContinent = (event, value) => {
+        console.log(value); // Logs the selected value
+    };
+
     return (
-            <div className="continentAutocomplete">
+        <div className="continentAutocomplete">
             <Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={continents}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Choose a continent" InputLabelProps={{ style: { color: 'white' } }}/>}
+                disablePortal
+                id="combo-box-demo"
+                options={continents}
+                sx={{ width: 350 }}
+                onChange={selectedContinent}
+                renderInput={(params) => <TextField {...params} label="Choose a continent" InputLabelProps={{ style: { color: 'white' } }}/>}
             />
-            </div>
-        );
-    }
-        
+        </div>
+    );
+}
+
 const continents = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
