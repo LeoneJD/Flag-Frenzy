b7b4d521dd0c05222df18128af2e7619b5c15d64
Aftab Ahmad <aftabahmad.farcy.888@gmail.com>
2024-03-05 22:50:22 +0000

    I have created SinglCountry.jsx file for single flag and nation info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+//#
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Countries from "./components/Countries1";
+import SingleCountry from "./components/SingleCountry";
+//#
+
+
 import { useState } from 'react'
 import './App.css'
-
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { motion } from "framer-motion"
@@ -14,14 +21,32 @@ function App() {
   const [score, setScore] = useState(0);
 
   return (
-    <>
+    <div>
+    
+    //#
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Countries />}></Route>
+        <Route path="/:name" element={<SingleCountry />}></Route>
+      </Routes>
+
+      //#
+      
+    </BrowserRouter>
+
+    {/* <>
       <NavBar counter={counter} score={score}/>
       <div>
         < ContinentSelector setScore={setScore} setCounter={setCounter} score={score} counter={counter}/>
       </div>
       <Footer />
 
-    </>
+    </> */}
+    </div>
+
+   
+    
   )
 }
 
