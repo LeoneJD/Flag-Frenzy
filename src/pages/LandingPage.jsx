017e12c9340353d4b075007aa7dbdb017693766a
Melisa <andream_dearth@hotmail.com>
2024-03-06 17:14:16 +0000

    Adding scale to the text with framer motion and shadow to the flags

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,16 @@
 import ContinentSelector from '../components/ContinentSelector.jsx';
+import { motion } from 'framer-motion';
 
 const LandingPage = ({ counter, score, setScore, setCounter }) => {
 
     return (
     <>
 
-        <h2>Let's see your knowledge of country flags! </h2>
+        <motion.h2 
+            whileHover={{ scale: 1.6 }} 
+        >
+            Let's see your knowledge of country flags!
+        </motion.h2>
         <div>
             <ContinentSelector setScore={setScore} setCounter={setCounter} score={score} counter={counter}/>
         </div>
