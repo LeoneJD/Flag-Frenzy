4ae6c128654f0600d8ec0b7374e0d6111ff4a13f
Leone Dale <dalejleone@gmail.com>
2024-03-07 18:35:27 +0000

    Added joke API to flags.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,31 +18,6 @@ function App() {
   const [counter, setCounter] = useState(0);
   const [score, setScore] = useState(0);
 
-async function randomjoke() {
-  const url = 'https://joke-box.p.rapidapi.com/api/v1/jokes/DadJokes';
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '4e3e1380e7msh1244316b160181bp1c5e19jsn8f3e50c9ecab',
-      'X-RapidAPI-Host': 'joke-box.p.rapidapi.com'
-    }
-  };
-  
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    console.log(result);
-    console.log(result[0]);
-    console.log(result[0].Joke);
-  } catch (error) {
-    console.error(error);
-  }
-}
-  
-randomjoke() 
-
-
-
   return (
 
       <Router >
@@ -62,4 +37,3 @@ randomjoke()
 }
 
 export default App
-
